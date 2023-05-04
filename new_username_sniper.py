e9998db3ca6c2565d93fbc50dcc23b6af001c452
SL4F <52598005+SL4F@users.noreply.github.com>
2023-05-04 13:29:45 +1000

    clean-up code

--- new_username_sniper.py
+++ new_username_sniper.py
@@ -15,13 +15,17 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.60 Chrome/108.0.5359.215 Electron/22.3.2 Safari/537.36'
 }    
 payload = {'username': username}
-r = requests.post('https://discord.com/api/v9/users/@me/pomelo', headers=headers, json=payload, timeout=10)
-while r.status_code != 200:
+while True:
     r = requests.post('https://discord.com/api/v9/users/@me/pomelo', headers=headers, json=payload, timeout=10)
     if r.status_code == 200:
-        print(f"{Fore.RED}▸ {Style.BRIGHT}{Fore.BLACK}{current_time()}{Style.RESET_ALL}{Fore.GREEN} [SUCCESS] {Fore.YELLOW}Claimed username.\n"+Fore.RESET)
+        print(f"{Fore.RED}▸ {Style.BRIGHT}{Fore.BLACK}{current_time()}{Style.RESET_ALL}{Fore.GREEN} [SUCCESS] {Fore.YELLOW}200 - Claimed username.\n"+Fore.RESET)
+        break
+    elif r.status_code == 401:
+        print(f"{Fore.RED}▸ {Style.BRIGHT}{Fore.BLACK}{current_time()}{Style.RESET_ALL}{Fore.RED} [ERROR] {Fore.YELLOW}401 - Unauthorized\n"+Fore.RESET)
+        wait = random.randrange(3600, 5400)     
+        time.sleep(wait)
     elif r.status_code == 429:
-        print(f"{Fore.RED}▸ {Style.BRIGHT}{Fore.BLACK}{current_time()}{Style.RESET_ALL}{Fore.RED} [ERROR] {Fore.YELLOW}Too many attempts.\n"+Fore.RESET)
+        print(f"{Fore.RED}▸ {Style.BRIGHT}{Fore.BLACK}{current_time()}{Style.RESET_ALL}{Fore.RED} [ERROR] {Fore.YELLOW}429 - Too many attempts.\n"+Fore.RESET)
         wait = random.randrange(3600, 5400)     
         time.sleep(wait)
     else:
